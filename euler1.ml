19879225fd566a0574e1463ca1cc922cc5152dfb
Bipra  Kundu <biprakundu@Bipras-MBP.home>
2017-10-09 01:24:53 -0400

    Euler 1

--- euler1.ml
+++ euler1.ml
@@ -5,4 +5,4 @@ let rec mult_of_three_five start acc =
     if (n mod 3 = 0 || n mod 5 = 0) then mult_of_three_five (n - 1) (acc + n)
     else mult_of_three_five (n - 1) acc
 
-let answer = mult_of_three_five 5 0
+let answer = mult_of_three_five 999 0

63dd597eb3251be5e616655cfcc7efcd5d302cef
Bipra  Kundu <biprakundu@Bipras-MBP.home>
2017-10-09 01:24:07 -0400

    Euler 1

---
+++ euler1.ml
@@ -0,0 +1,8 @@
+let rec mult_of_three_five start acc =
+  match start with
+  | 0 -> acc
+  | n ->
+    if (n mod 3 = 0 || n mod 5 = 0) then mult_of_three_five (n - 1) (acc + n)
+    else mult_of_three_five (n - 1) acc
+
+let answer = mult_of_three_five 5 0

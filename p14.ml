c67ba660b2c13c90f6bd824f2d3de4d4f4394ed1
Bipra Kundu <biprakundu@Bipras-MacBook-Pro.local>
2017-10-10 16:34:06 -0400

    fold left and fold right methods

--- p14.ml
+++ p14.ml
@@ -2,4 +2,10 @@
 
 let rec duplicate = function
   | [] -> []
-  | h :: t -> h :: h :: duplicate t 
+  | h :: t -> h :: h :: duplicate t
+
+(* using fold_left *)
+let duplicate lst = List.fold_left (fun a x -> x :: x :: a) [] (List.rev lst)
+
+(* using fold_right *)
+let duplicate lst = List.fold_right (fun x a -> x :: x :: a) lst []
--- p15.ml
+++ p15.ml
@@ -1,3 +1,7 @@
 (* Replicate the elements of a list a given number of times *)
 
+let replicate lst n =
+  let rec prepend n acc a =
+    if n = 0 then acc else prepend (n - 1) (a :: acc) a  in
+  List.fold_left (prepend n) [] (List.rev lst)
   
